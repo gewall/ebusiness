a9e577471a4d06d1447ac03171a0aa3594dab9cd
gewall <alginugraha21@gmail.com>
2022-10-21 17:07:14 +0700

    init

---
+++ script.js
@@ -0,0 +1,111 @@
+const catalogList = [
+  {
+    id: 0,
+    nama: "Cermin",
+    deskripsi: "abcdefghijk",
+    harga: 20000,
+  },
+  {
+    id: 1,
+    nama: "Cermin dua",
+    deskripsi: "abcdelmnopq",
+    harga: 20000,
+  },
+];
+
+const showTopProductList = () => {
+  const topProductId = document.getElementById("list-produk-unggulan");
+  const tes = catalogList.map((item) => {
+    return `
+    <div class="col">
+          <div class="d-flex flex-column x-card" style="width: 12rem;">
+              <img src="https://via.placeholder.com/1920x1200.png?text=${
+                item.nama
+              }" class="card-img-top "
+                  style="height: 280px; object-fit: cover;" alt="...">
+              <div class="card-body x-card-body my-2">
+                  <h5 class="fs-5">${item.nama}</h5>
+                  <h4 class="fs-4 text-muted" style="word-break:break-word;">${
+                    item.deskripsi
+                  }</h4>
+                  <h4 class="fs-5">${new Intl.NumberFormat("ID-id", {
+                    style: "currency",
+                    currency: "IDR",
+                  }).format(item.harga)}</h4>
+                  <button class="text-dark btn btn-transparent "><span class="text-decoration-underline">Tambah
+                          Keranjang</span></button>
+              </div>
+          </div>
+      </div>
+    `;
+  });
+  console.log(tes);
+  topProductId.innerHTML += tes;
+};
+
+const showOurProductList = () => {
+  const ourProductId = document.getElementById("list-produk-kami");
+  catalogList.map((item) => {
+    ourProductId.innerHTML += `
+    <div class="col">
+          <div class="d-flex flex-column x-card" style="width: 12rem;">
+              <img src="https://via.placeholder.com/1920x1200.png?text=${
+                item.nama
+              }" class="card-img-top "
+                  style="height: 280px; object-fit: cover;" alt="...">
+              <div class="card-body x-card-body my-2">
+                  <h5 class="fs-5">${item.nama}</h5>
+                  <h4 class="fs-4 text-muted"  style="word-break:break-word;">${
+                    item.deskripsi
+                  }</h4>
+                  <h4 class="fs-5">${new Intl.NumberFormat("ID-id", {
+                    style: "currency",
+                    currency: "IDR",
+                  }).format(item.harga)}</h4>
+                  <button class="text-dark btn btn-transparent "><span class="text-decoration-underline">Tambah
+                          Keranjang</span></button>
+              </div>
+          </div>
+      </div>
+    `;
+  });
+};
+
+const showHeroProductList = () => {
+  const heroProductId = document.getElementById("list-produk-hero");
+  catalogList.map((item) => {
+    heroProductId.innerHTML += `
+    <div class="col">
+          <div class="d-flex flex-column x-card bg-white" style="width: 12rem;">
+              <img src="https://via.placeholder.com/1920x1200.png?text=${
+                item.nama
+              }" class="card-img-top "
+                  style="height: 280px; object-fit: cover;" alt="...">
+              <div class="card-body x-card-body my-2">
+                  <h5 class="fs-5">${item.nama}</h5>
+                  <h4 class="fs-4 text-muted"  style="word-break:break-word;">${
+                    item.deskripsi
+                  }</h4>
+                  <h4 class="fs-5">${new Intl.NumberFormat("ID-id", {
+                    style: "currency",
+                    currency: "IDR",
+                  }).format(item.harga)}</h4>
+                  <button class="text-dark btn btn-transparent "><span class="text-decoration-underline">Tambah
+                          Keranjang</span></button>
+              </div>
+          </div>
+      </div>
+    `;
+  });
+};
+
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
+
+showTopProductList();
+showOurProductList();
+showHeroProductList();
---
+++ theme.js
@@ -0,0 +1,34 @@
+let data = {
+  scrollPos: 0,
+};
+
+document.onreadystatechange = () => {
+  dataset();
+};
+
+const dataset = () => {
+  window.onscroll = (e) => {
+    data.scrollPos = Math.ceil(window.scrollY);
+
+    navbarHandler();
+  };
+};
+
+const navbarHandler = () => {
+  const navId = document.getElementById("navbar");
+  const navText = document.getElementsByClassName("nav-text");
+
+  if (data.scrollPos > 5) {
+    navId.classList.add("bg-light");
+    navId.classList.remove("bg-transparent");
+    for (let i = 0; i < navText.length; i++) {
+      navText[i].style.color = "rgba(0,0,0,.77)";
+    }
+  } else {
+    navId.classList.remove("bg-light");
+    navId.classList.add("bg-transparent");
+    for (let i = 0; i < navText.length; i++) {
+      navText[i].style.color = "white";
+    }
+  }
+};
